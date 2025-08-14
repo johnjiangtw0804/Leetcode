3924a6a5029e73ed63e6cbc8b23e5b3637435796
Jonathan Jiang <jonathan.jiang01@sjsu.edu>
2025-08-14 11:59:37 +0800

    Update

---
+++ Daily/2264. Largest 3-Same-Digit Number in String/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String largestGoodInteger(String num) {
+        char[] s = num.toCharArray();
+        char best = 0; // 0 代表尚未找到；實際字元都 >= '0' (48)
+
+        for (int i = 0; i + 2 < s.length; i++) {
+            char c = s[i];
+            if (c == s[i + 1] && c == s[i + 2]) { // 連續三個一樣
+                if (c > best)
+                    best = c; // 更新最大 digit
+            }
+        }
+
+        return best == 0 ? "" : String.valueOf(best).repeat(3);
+    }
+}
